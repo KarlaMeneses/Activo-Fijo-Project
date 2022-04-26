027267fb95118351880a3e8b406ab9ff9d4aea9a
LeoMogiano <leomogiano@outlook.com>
2022-04-26 17:40:04 -0400

    Apartado de Usuario y Roles

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +23,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('roles', RoleController::class)->names('roles');
+Route::resource('users', UserController::class)->names('users');
