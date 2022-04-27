b83b1b601eddf1ade9376f18c4b076a7714b1fa4
JuanitaBP <juanitapadilla483@gmail.com>
2022-04-27 15:58:00 -0400

    creacion de show blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,6 +63,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    
     public function show($id)
     {
         $user = User::find($id);
@@ -77,6 +78,7 @@ class UserController extends Controller
      */
     public function edit($id)
     {
+        $roles = Role::all();
         $user = User::find($id);
         $rol = DB::table('model_has_roles')->where('model_id', $user->id)->first();
         $rol_name = DB::table('roles')->where('id', $rol->role_id)->first();
