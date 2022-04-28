519418f2ee28ed99c4cea9a9479eb5b8f3e80b5d
KarlaMeneses <karlamenesses19@gmail.com>
2022-04-28 00:13:09 -0400

    bajando cambios

--- database/migrations/2022_04_28_024813_create_notas_table.php
+++ database/migrations/2022_04_28_024813_create_notas_table.php
@@ -15,6 +15,23 @@ class CreateNotasTable extends Migration
     {
         Schema::create('notas', function (Blueprint $table) {
             $table->id();
+            //nota compra
+            $table->integer('unidad');
+            $table->string('concepto');
+            $table->integer('precio_uni');
+            $table->integer('importe');
+            $table->string('condicion_pago');
+            $table->date('fecha_envio');
+            $table->date('fecha_entrega');
+            $table->String('lugar_entrega');
+            //nota venta
+            $table->integer('nro_egreso');
+            $table->string('almacen');
+            $table->string('entregado_a');
+            $table->unsignedBigInteger('id_responsable');
+            $table->string('orden');
+            $table->foreign('id_responsable')->on('users')->references('id')->onDelete('cascade');
+            
             $table->timestamps();
         });
     }
