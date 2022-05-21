0b992c811abaf34e2b9bbc5128469f022b211147
angelicamiranda <angelicamirandau@gmail.com>
2022-05-20 22:39:08 -0400

    lol

--- database/seeders/DetalleFacturaSeeder.php
+++ database/seeders/DetalleFacturaSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DetalleFacturaSeeder extends Seeder
 {
     /**
@@ -13,6 +13,27 @@ class DetalleFacturaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('detallefactura')->insert([
+            [
+                //nota compra
+                'cantidad' => 2,
+                'descuento' => 1000.00,
+                'articulo' => 'Laptop',
+                'codigo' => 'A1-001',
+                'valor_unitario' => 5000.00,
+                'valor_total' => 9000.00,
+                'idfactura' => 1, //id de la nota
+            ],
+            [
+                'cantidad' => 1,
+                'descuento' => 0.00,
+                'articulo' => 'Laptop',
+                'codigo' => 'A1-001',
+                'valor_unitario' => 2000.00,
+                'valor_total' => 2000.00,
+                'idfactura' => 1, //id de la nota
+            ],
+            
+        ]);
     }
 }
