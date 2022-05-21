f0256aed3e222c00b4779cb57ebf4a65c3f16d0b
angelicamiranda <angelicamirandau@gmail.com>
2022-05-20 22:24:09 -0400

    lol

--- database/seeders/facturaSeeder.php
+++ database/seeders/facturaSeeder.php
@@ -18,13 +18,13 @@ class facturaSeeder extends Seeder
             [   //Factura compra
                 [
                     'tipo' => 'compra',
-                    'vendedor' => 'Juan Pérez',
+                    'vendedor' => 'Nippon Computers',
                     'nit' => '12922186',
                     'idcomprador' => '1',
                     'ciudad' => 'Santa Cruz de la Sierra',
                     'direccion' => 'Comercial chiriguano p/4 caseta # 123',
-                    'telefono' => 6235093,
-                    'email' => 'juanperez@gmail.com',
+                    'telefono' => 33517662,
+                    'email' => 'nipponcomputers@gmail.com',
                     'formadepago' => 'Cheque',
                     'totalneto' => 20400.00,
                     'fechaemisión' => '2022-02-20',
@@ -37,13 +37,13 @@ class facturaSeeder extends Seeder
                 // Factura compra 2
                 [
                     'tipo' => 'compra',
-                    'vendedor' => 'Maria Montaño',
+                    'vendedor' => 'Mi Kasa Muebles',
                     'nit' => '5892486',
                     'idcomprador' => '2',
                     'ciudad' => 'Santa Cruz de la Sierra',
                     'direccion' => '4to anillo entre Av. Paragua #591',
-                    'telefono' => 6235093,
-                    'email' => 'mariamontaño@gmail.com',
+                    'telefono' => 61552473,
+                    'email' => 'mikasamuebles@gmail.com',
                     'formadepago' => 'Efectivo',
                     'totalneto' => 1929.00,
                     'fechaemisión' => '2022-04-16',
@@ -56,16 +56,16 @@ class facturaSeeder extends Seeder
                 // Factura compra 3---------------------------------------------------------------------
                 [
                     'tipo' => 'compra',
-                    'vendedor' => 'Nathaly Rios',
+                    'vendedor' => 'Muebles Cabrera',
                     'nit' => '981892',
                     'idcomprador' => '3',
                     'ciudad' => 'Cochabamba',
-                    'direccion' => 'Av. Melchor Pérez de Olguín N°984 esquina',
-                    'telefono' => 71005231,
+                    'direccion' => 'Av. América 955',
+                    'telefono' => 62635668,
                     'email' => 'nathalyrios@gmail.com',
                     'formadepago' => 'Transferencia Bancaria',
                     'totalneto' => 500.00,
-                    'fechaemisión' => '2022-02-20',
+                    'fechaemisión' => '2022-04-28',
                     'iva' => 65.00,
                     'totaliva' => 565.00,
                     'referencia' => 'Producto elaborado y nuevo',
@@ -75,58 +75,126 @@ class facturaSeeder extends Seeder
                 // Factura Compra 4---------------------------------------------------------------------
                 [
                     'tipo' => 'compra',
-                    'vendedor' => 'Marcos Aguilar',
+                    'vendedor' => 'Gac Motor Bolivia',
                     'nit' => '5897103',
                     'idcomprador' => '1',
                     'ciudad' => 'Santa Cruz de la Sierra',
                     'direccion' => 'Av. Doble vía la Guaria, casi 4to anillo',
-                    'telefono' => 68948267,
-                    'email' => 'marcosaguilar@gmail.com',
+                    'telefono' => 72168231,
+                    'email' => 'gacmotor@gmail.com',
                     'formadepago' => 'Cheque',
-                    'totalneto' => 500.00,
-                    'fechaemisión' => '2022-02-20',
-                    'iva' => 65.00,
-                    'totaliva' => 565.00,
-                    'referencia' => 'Producto elaborado y nuevo',
+                    'totalneto' => 118300.00,
+                    'fechaemisión' => '2022-03-11',
+                    'iva' => 15379.00,
+                    'totaliva' => 133679.00,
+                    'referencia' => 'Nuevo',
                     'foto' => 'https://firebasestorage.googleapis.com/v0/b/imagenes-972f4.appspot.com/o/notas%2Fcompras%2F3.jpeg?alt=media&token=b6440cf8-f26a-4e9e-af88-8e7224dfe52e',
 
                 ],
                 // Factura compra 5---------------------------------------------------------------------
                 [
                     'tipo' => 'compra',
-                    'vendedor' => 'Nathaly Rios',
+                    'vendedor' => 'Mega Sistems Comp',
                     'nit' => '981892',
                     'idcomprador' => '3',
-                    'ciudad' => 'Cochabamba',
-                    'direccion' => 'Av. Melchor Pérez de Olguín N°984 esquina',
-                    'telefono' => 71005231,
-                    'email' => 'nathalyrios@gmail.com',
-                    'formadepago' => 'Transferencia Bancaria',
-                    'totalneto' => 500.00,
-                    'fechaemisión' => '2022-02-20',
-                    'iva' => 65.00,
-                    'totaliva' => 565.00,
-                    'referencia' => 'Producto elaborado y nuevo',
+                    'ciudad' => 'Santa Cruz de la Sierra',
+                    'direccion' => 'Comercial Chiriguano, Av. Roque Aguilera',
+                    'telefono' =>  77658209,
+                    'email' => 'megasistems@gmail.com',
+                    'formadepago' => 'efectivo',
+                    'totalneto' => 4498.20,
+                    'fechaemisión' => '2022-01-18',
+                    'iva' => 584.77,
+                    'totaliva' => 5082.97,
+                    'referencia' => 'Producto nuevo',
                     'foto' => 'https://firebasestorage.googleapis.com/v0/b/imagenes-972f4.appspot.com/o/notas%2Fcompras%2F3.jpeg?alt=media&token=b6440cf8-f26a-4e9e-af88-8e7224dfe52e',
 
                 ],
             ]
         );
 
-        DB::table('notas')->insert(
-            [   //  nota venta
+       /* DB::table('facturas')->insert(
+            [   //Factura venta
                 [
-
                     'tipo' => 'venta',
-                    'adquirente' => 'Juan Perez',
-                    'telefono' => 76320594,
-                    'fecha_venta' => '2022-02-20',
-                    'encargado' => 'Esteban Flores',
-                    'cargo' => 'auxiliar de ventas',
-                    'totales' => 450,
-                ],
+                    'comprador' => 'Juanita',
+                    'nit' => '5892486',
+                    'idvendedor' => '2',
+                    'ciudad' => 'Santa Cruz de la Sierra',
+                    'telefono' => 61552473,
+                    'formadepago' => 'Efectivo',
+                    'totalneto' => 1929.00,
+                    'fechaemisión' => '2022-04-16',
+                    'iva' => 250.77,
+                    'totaliva' => 2179.77,
+                    'foto' => 'https://firebasestorage.googleapis.com/v0/b/imagenes-972f4.appspot.com/o/notas%2Fcompras%2F3.jpeg?alt=media&token=b6440cf8-f26a-4e9e-af88-8e7224dfe52e',
 
+                ],
+                // Factura compra 2
+                [
+                    'tipo' => 'venta',
+                    'comprador' => 'Mi Kasa Muebles',
+                    'nit' => '5892486',
+                    'idvendedor' => '1',
+                    'ciudad' => 'Santa Cruz de la Sierra',
+                    'telefono' => 61552473,
+                    'formadepago' => 'Efectivo',
+                    'totalneto' => 1929.00,
+                    'fechaemisión' => '2022-04-16',
+                    'iva' => 250.77,
+                    'totaliva' => 2179.77,
+                    'foto' => 'https://firebasestorage.googleapis.com/v0/b/imagenes-972f4.appspot.com/o/notas%2Fcompras%2F3.jpeg?alt=media&token=b6440cf8-f26a-4e9e-af88-8e7224dfe52e',
+
+                ],
+                // Factura compra 3---------------------------------------------------------------------
+                [
+                    'tipo' => 'venta',
+                    'comprador' => 'Mi Kasa Muebles',
+                    'nit' => '5892486',
+                    'idvendedor' => '3',
+                    'ciudad' => 'Santa Cruz de la Sierra',
+                    'telefono' => 61552473,
+                    'formadepago' => 'Efectivo',
+                    'totalneto' => 1929.00,
+                    'fechaemisión' => '2022-04-16',
+                    'iva' => 250.77,
+                    'totaliva' => 2179.77,
+                    'foto' => 'https://firebasestorage.googleapis.com/v0/b/imagenes-972f4.appspot.com/o/notas%2Fcompras%2F3.jpeg?alt=media&token=b6440cf8-f26a-4e9e-af88-8e7224dfe52e',
+
+                ],
+                // Factura Compra 4---------------------------------------------------------------------
+                [
+                    'tipo' => 'venta',
+                    'comprador' => 'Mi Kasa Muebles',
+                    'nit' => '5892486',
+                    'idvendedor' => '2',
+                    'ciudad' => 'Santa Cruz de la Sierra',
+                    'telefono' => 61552473,
+                    'formadepago' => 'Efectivo',
+                    'totalneto' => 1929.00,
+                    'fechaemisión' => '2022-04-16',
+                    'iva' => 250.77,
+                    'totaliva' => 2179.77,
+                    'foto' => 'https://firebasestorage.googleapis.com/v0/b/imagenes-972f4.appspot.com/o/notas%2Fcompras%2F3.jpeg?alt=media&token=b6440cf8-f26a-4e9e-af88-8e7224dfe52e',
+
+                ],
+                // Factura compra 5---------------------------------------------------------------------
+                [
+                    'tipo' => 'venta',
+                    'comprador' => 'Mi Kasa Muebles',
+                    'nit' => '5892486',
+                    'idvendedor' => '3',
+                    'ciudad' => 'Santa Cruz de la Sierra',
+                    'telefono' => 61552473,
+                    'formadepago' => 'Efectivo',
+                    'totalneto' => 1929.00,
+                    'fechaemisión' => '2022-04-16',
+                    'iva' => 250.77,
+                    'totaliva' => 2179.77,
+                    'foto' => 'https://firebasestorage.googleapis.com/v0/b/imagenes-972f4.appspot.com/o/notas%2Fcompras%2F3.jpeg?alt=media&token=b6440cf8-f26a-4e9e-af88-8e7224dfe52e',
+
+                ],
             ]
-        );
+        );*/
     }
 }
