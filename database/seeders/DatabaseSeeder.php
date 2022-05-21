97d1ef00572b0dbbddbc05bf344fe13f429dcd19
angelicamiranda <angelicamirandau@gmail.com>
2022-05-20 23:29:53 -0400

    lol

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
         $this->call(facturaSeeder::class);
         /*$this->call(activofijoSeeder::class);*/
         $this->call(DetallenotaSeeder::class);
+        $this->call(detallefacturaSeeder::class);
         $this->call(revalorizacionSeeder::class);
 
     }
