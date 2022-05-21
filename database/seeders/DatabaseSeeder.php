34caab282735a2f9cad111338eb0806afb989426
KarlaMeneses <karlamenesses19@gmail.com>
2022-05-21 00:28:11 -0400

    soy yo otraves

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
         /*$this->call(activofijoSeeder::class);*/
         $this->call(DetallenotaSeeder::class);
         $this->call(revalorizacionSeeder::class);
+        $this->call(DetalleFacturaSeeder::class);
 
     }
 }
