4536b03d96e46914fe3559ef000b06d26b84f0a6
pericena12@gmail.com <pericena12@gmail.com>
2022-06-25 21:20:36 -0400

    hi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,77 +28,59 @@
         <img style="margin:auto;display:block; padding-top: 6rem" width="400rem"
             src="https://anepsa.com.mx/wp-content/uploads/2021/05/SAAF-logo-con-tipografi%CC%81a.png" alt="">
     </div>
-<!--
-    <div class="container">
-        <nav>
-            <ul class="mcd-menu">
-                <li>
-                    <a href="">
-                        <i class="fa fa-home"></i>
-                        <strong>Home</strong>
-                        <small>sweet home</small>
-                    </a>
-                </li>
-                <li>
-                    <a href="" class="active">
-                        <i class="fa fa-edit"></i>
-                        <strong>About us</strong>
-                        <small>sweet home</small>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-gift"></i>
-                        <strong>Features</strong>
-                        <small>sweet home</small>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-globe"></i>
-                        <strong>News</strong>
-                        <small>sweet home</small>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-comments-o"></i>
-                        <strong>Blog</strong>
-                        <small>what they say</small>
-                    </a>
-                    <ul>
-                        <li><a href="#"><i class="fa fa-globe"></i>Mission</a></li>
-                        <li>
-                            <a href="#"><i class="fa fa-group"></i>Our Team</a>
-                            <ul>
-                                <li><a href="#"><i class="fa fa-female"></i>Leyla Sparks</a></li>
-                                <li>
-                                    <a href="#"><i class="fa fa-male"></i>Gleb Ismailov</a>
-                                    <ul>
-                                        <li><a href="#"><i class="fa fa-leaf"></i>About</a></li>
-                                        <li><a href="#"><i class="fa fa-tasks"></i>Skills</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#"><i class="fa fa-female"></i>Viktoria Gibbers</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#"><i class="fa fa-trophy"></i>Rewards</a></li>
-                        <li><a href="#"><i class="fa fa-certificate"></i>Certificates</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="">
-                        <i class="fa fa-envelope-o"></i>
-                        <strong>Contacts</strong>
-                        <small>drop a line</small>
-                    </a>
-                </li>
 
-            </ul>
-        </nav>
+<div class="container mt-2">
+<!--   <div class="card card-block mb-2">
+    <h4 class="card-title">Card 1</h4>
+    <p class="card-text">Welcom to bootstrap card styles</p>
+    <a href="#" class="btn btn-primary">Submit</a>
+  </div>   -->
+  <div class="row">
+    <div class="col-md-3 col-sm-6">
+      <div class="card card-block">
+      <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+    <img src="https://static.pexels.com/photos/7096/people-woman-coffee-meeting.jpg" alt="Photo of sunset">
+        <h5 class="card-title mt-3 mb-3">Sierra Web Development • Owner</h5>
+        <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p>
+  </div>
     </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="card card-block">
+      <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+    <img src="https://static.pexels.com/photos/7357/startup-photos.jpg" alt="Photo of sunset">
+        <h5 class="card-title  mt-3 mb-3">ProVyuh</h5>
+        <p class="card-text">This is a company that builds websites, web .</p>
+  </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="card card-block">
+      <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+    <img src="https://static.pexels.com/photos/262550/pexels-photo-262550.jpeg" alt="Photo of sunset">
+        <h5 class="card-title  mt-3 mb-3">ProVyuh</h5>
+        <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p>
+  </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="card card-block">
+      <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+    <img src="https://static.pexels.com/photos/326424/pexels-photo-326424.jpeg" alt="Photo of sunset">
+        <h5 class="card-title  mt-3 mb-3">ProVyuh</h5>
+        <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p>
+  </div>
+    </div>
+  </div>
 
--->
+</div>
+
+
+
+<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  intent="WELCOME"
+  chat-title="bots"
+  agent-id="86938b5f-1e37-43dc-9f38-1bd5322b1eb7"
+  language-code="es"
+></df-messenger>
 
 
 
@@ -109,6 +91,7 @@
 @section('css')
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bot.css') }}">
 @stop
 
 @section('js')
