6ced1d44366a2bef581e6f9abfb3475f668b4d6a
Jos317 <jchilelaime38@gmail.com>
2022-07-03 21:58:41 -0400

    Login Activos Facturas

--- app/Http/Controllers/Api/ActivoController.php
+++ app/Http/Controllers/Api/ActivoController.php
@@ -20,4 +20,22 @@ class ActivoController extends Controller
             return response()->json(['mensaje' => $e->getMessage()], 500);
         }
     }
+
+    public function obtenerActivo(Request $request)
+    {
+        try {
+            $p = Activofijo::where('codigo', $request->codigo)->first();
+            $activo = Activofijo::join('ubicacion', 'activosfijo.id_ubicacion', 'ubicacion.id')
+            ->join('departamentos', 'ubicacion.id_departamento', 'departamentos.id')
+            ->join('depreciaciones', 'activosfijo.id_depreciacion', 'depreciaciones.id')
+            ->select('activosfijo.codigo', 'activosfijo.nombre', 'activosfijo.detalle', 'activosfijo.tipo', 'activosfijo.fecha_ingreso', 'activosfijo.costo', 'activosfijo.estado', 'activosfijo.proveedor', 'activosfijo.d_anual',
+            'ubicacion.pais as ubicacion_pais', 'ubicacion.ciudad as ubicacion_ciudad', 'ubicacion.edificio as ubicacion_edificio', 'departamentos.nombre as departamento_nombre','depreciaciones.nombre as depreciacion_nombre', 'depreciaciones.descripcion as depreciacion_descripcion',
+            'depreciaciones.vida_util as depreciacion_vidaUtil', 'depreciaciones.valor_residual as depreciacion_valorResidual')
+            ->where('codigo', $request->codigo)
+            ->first();
+            return response()->json(['mensaje' => 'Consulta exitosa', 'data' => $activo], 200);
+        } catch (\Exception $e) {
+            return response()->json(['mensaje' => $e->getMessage()], 500);
+        }
+    }
 }
