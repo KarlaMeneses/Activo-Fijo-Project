2dcf32dc581834ff95f74625eb245ee5bdfc9b31
pericena12@gmail.com <pericena12@gmail.com>
2022-07-21 01:25:11 -0400

    revalorización

--- resources/views/revalorizacion/create.blade.php
+++ resources/views/revalorizacion/create.blade.php
@@ -1,58 +1,96 @@
 @extends('adminlte::page')
 
 @section('title', 'Activo Fijo')
-
 @section('content_header')
-    <h1>Crear Revalorizacion</h1>
+<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-storage.js"></script>
+
+<h1>Crear Revalorizacion</h1>
+@stop
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/subir.css') }}">
 @stop
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <form action="{{ route('revalorizacion.store') }}" method="post" novalidate>
-                @csrf
-                <label for="tiempo_vida">tiempo_vida</label>
-                <input type="text" name="tiempo_vida" class="form-control"> <br>
-                @error('tiempo_vida')
-                    <small class="text-danger">*{{ $message }}</small>
-                    <br><br>
-                @enderror
+<div class="card">
+    <div class="card-body">
+        <form action="{{ route('revalorizacion.store') }}" method="post" novalidate>
+            @csrf
+            @section('js')
+            <script src="{{ asset('js/revalorizacion.js') }}"></script>
 
-                <label for="valor">valor</label>
-                <input type="text" name="valor" class="form-control"> <br>
-                @error('valor')
-                    <small class="text-danger">*{{ $message }}</small>
-                    <br><br>
-                @enderror
+            @endsection
 
-                <label for="valor">costo</label>
-                <input type="decimal" name="monto" class="form-control"> <br>
-                @error('monto')
-                    <small class="text-danger">*{{ $message }}</small>
-                    <br><br>
-                @enderror
+            <center>
+                {{-- separador --}}
+                <div class="form-group col-md-3">
+                    <img width="200" height="200" class="img-circle" id="foto">
+                    <div class="custom-input-file">
+                        <input type="file" id="file" accept="image/*" class="input-file" value="">
+                        <i class="fas fa-file-upload"></i> Subir Foto...
+                    </div>
+                    <div class="col-12" id="app" style="text-align:center;">
+                        <progress id="progress_bar" value="0" max="100"></progress>
+                        <input type="hidden" value="{{ old('foto') }}" name="foto" id="fotov" title="foto" placeholder="https://example.com" list="defaultURLs" class="focus border-dark  form-control" oninvalid="this.setCustomValidity('Please match the requested format')">
+                    </div>
+                    @error('foto')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+            </center>
 
-                <label for="id_activo">id_activo</label>
-                <input type="text" name="id_activo" class="form-control"> <br>
-                @error('id_activo')
-                    <small class="text-danger">*{{ $message }}</small>
-                    <br><br>
-                @enderror
+            <label for="tiempo_vida">tiempo_vida</label>
+            <input type="text" name="tiempo_vida" class="form-control"> <br>
+            @error('tiempo_vida')
+            <small class="text-danger">*{{ $message }}</small>
+            <br><br>
+            @enderror
 
-                <button class="btn btn-danger btn-sm" type="submit">Crear Departamento</button>
-                <a class="btn btn-primary btn-sm" href="{{ route('revalorizacion.index') }}">Volver</a>
-            </form>
+            <label for="valor">valor</label>
+            <input type="text" name="valor" class="form-control"> <br>
+            @error('valor')
+            <small class="text-danger">*{{ $message }}</small>
+            <br><br>
+            @enderror
 
-        </div>
+            <label for="valor">costo</label>
+            <input type="decimal" name="monto" class="form-control"> <br>
+            @error('monto')
+            <small class="text-danger">*{{ $message }}</small>
+            <br><br>
+            @enderror
+
+            <label for="id_activo">id_activo</label>
+            <input type="text" name="id_activo" class="form-control"> <br>
+            @error('id_activo')
+            <small class="text-danger">*{{ $message }}</small>
+            <br><br>
+            @enderror
+
+            <button class="btn btn-danger btn-sm" type="submit">Crear Departamento</button>
+            <a class="btn btn-primary btn-sm" href="{{ route('revalorizacion.index') }}">Volver</a>
+        </form>
+
     </div>
+</div>
+<div>
+    <df-messenger intent="WELCOME" chat-title="bots" agent-id="86938b5f-1e37-43dc-9f38-1bd5322b1eb7" language-code="es">
+
+</div>
+
+
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+<link rel="stylesheet" href="{{ asset('css/bot.css') }}">
+<link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
+
 @section('js')
-    <script>
-        console.log('Hi!');
-    </script>
+<script>
+    console.log('Hi!');
+
+</script>
 @stop
--- resources/views/revalorizacion/show.blade.php
+++ resources/views/revalorizacion/show.blade.php
@@ -6,138 +6,135 @@
 @stop
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            @if (session('success'))
-                <div class="alert alert-success" role="success">
-                    {{ session('success') }}
-                </div>
-            @endif
-
-            <div class="card">
-                <div class="card-header">
-                    <center>
-                        <h2 class="font-weight-bold px-2">Información de la Revalorizacion</h2>
-                    </center>
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="form-row">
-                            <div class="card-header form-group col-md-12">
-                                <h5 class="font-weight-bold px-2">DATOS DEL ACTIVO FIJO</h5>
-                            </div>
-                            @foreach ($activosfijo as $activo)
-                                @if ($activo->id == $revalorizacion->id_activo)
-                                    <div class="form-group col-md-6">
-                                        <label for="codigo">CODIGO DE ACTIVO</label>
-                                        <input type="text" name="codigo" class="form-control"
-                                            value="{{ $activo->codigo }}" disabled>
-
-                                    </div>
-
-                                    <div class="form-group col-md-6">
-                                        <label for="detalle">NOMBRE DEL ACTIVO</label>
-                                        <input type="text" name="detalle" class="form-control"
-                                            value="{{ $activo->nombre }}" disabled>
-                                    </div>
-
-                                    <div class="col-md-3">
-                                        <label for="fecha_ingreso">FECHA DE INGRESO DEL ACTIVO</label>
-                                        <input name="fecha_ingreso" type="date" class="form-control"
-                                            value="{{ $activo->fecha_ingreso }}" disabled>
-                                    </div>
-
-                                    <div class="col-md-3">
-                                        <label for="costo">COSTO ACTIVO</label>
-                                        <input name="costo" type="text" class="form-control"
-                                            value="{{ $activo->costo }}" disabled>
-                                    </div>
-
-                                    <div class="col-md-3">
-                                        <label for="v_actual">VALOR ACTUAL</label>
-                                        <input name="v_actual" type="text" class="form-control"
-                                            value="{{ $activo->v_actual }}" disabled>
-                                    </div>
-
-                                    <div class="col-md-3">
-                                        <label for="estado">ESTADO</label>
-                                        <input type="tel" name="estado" class="form-control"
-                                            value="{{ $activo->estado }}" disabled>
-                                    </div>
-                                @endif
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="form-row">
-                            <div class="card-header form-group col-md-12">
-                                <h5 class="font-weight-bold px-2">DATOS DEL ACTIVO REVALORIZADO POR EL EXPERTO</h5>
-                            </div>
-
-                            <div class="form-group col-md-4">
-                                <label for="descripcion">VALOR REVALUADO</label>
-                                <input type="text" name="descripcion" class="form-control"
-                                    value="{{ $revalorizacion->valor }} Bs" id="descripcion" disabled>
-                            </div>
-
-                            <div class="form-group col-md-4">
-                                <label for="nombre">VIDA UTIL REVALUADO</label>
-                                <input type="text" name="nombre" class="form-control"
-                                    value="{{ $revalorizacion->tiempo_vida }} años" id="nombre" disabled>
-                            </div>
-
-                            <div class="form-group col-md-4">
-                                <label for="estado">ESTADO</label>
-                                <input type="text" name="estado" class="form-control"
-                                    value="{{ $revalorizacion->estado }}" id="descripcion" disabled>
-                            </div>
-
-                            <div class="form-group col-md-4">
-                                <label for="estado">FECHA SOLICITUD</label>
-                                <input type="text" name="estado" class="form-control"
-                                    value="{{ $revalorizacion->created_at }}" id="descripcion" disabled>
-                            </div>
-
-                            <div class="form-group col-md-4">
-                                <label for="costo_revaluo">COSTO DE LA REVALUACION</label>
-                                <input type="text" name="costo_revaluo" class="form-control"
-                                    value="{{ $revalorizacion->costo_revaluo }} Bs" id="descripcion" disabled>
-                            </div>
-                        </div>
-                        <h3>BOTON PARA MOSTRAR IMAGEN DEL INFORME TECNICO </h3>
-                    </div>
-                </div>
-            </div>
-            <br>
-            <center>
-                <a class="btn btn-warning btb-sm text-light" href="{{ route('revalorizacion.index') }}">Volver</a>
-                <a href="{{ route('revalorizacion.edit', $revalorizacion->id) }}"
-                    class="btn btn-primary btb-sm text-light">
-                    Editar </a>
-            </center>
+<div class="card">
+    <div class="card-body">
+        @if (session('success'))
+        <div class="alert alert-success" role="success">
+            {{ session('success') }}
         </div>
+        @endif
+
+        <div class="card">
+            <div class="card-header">
+                <center>
+                    <h2 class="font-weight-bold px-2">Información de la Revalorizacion</h2>
+                </center>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="form-row">
+                        <div class="card-header form-group col-md-12">
+                            <h5 class="font-weight-bold px-2">DATOS DEL ACTIVO FIJO</h5>
+                        </div>
+                        @foreach ($activosfijo as $activo)
+                        @if ($activo->id == $revalorizacion->id_activo)
+                        <div class="form-group col-md-6">
+                            <label for="codigo">CODIGO DE ACTIVO</label>
+                            <input type="text" name="codigo" class="form-control" value="{{ $activo->codigo }}" disabled>
+
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="detalle">NOMBRE DEL ACTIVO</label>
+                            <input type="text" name="detalle" class="form-control" value="{{ $activo->nombre }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fecha_ingreso">FECHA DE INGRESO DEL ACTIVO</label>
+                            <input name="fecha_ingreso" type="date" class="form-control" value="{{ $activo->fecha_ingreso }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="costo">COSTO ACTIVO</label>
+                            <input name="costo" type="text" class="form-control" value="{{ $activo->costo }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="v_actual">VALOR ACTUAL</label>
+                            <input name="v_actual" type="text" class="form-control" value="{{ $activo->v_actual }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="estado">ESTADO</label>
+                            <input type="tel" name="estado" class="form-control" value="{{ $activo->estado }}" disabled>
+                        </div>
+                        @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="form-row">
+                        <div class="card-header form-group col-md-12">
+                            <h5 class="font-weight-bold px-2">DATOS DEL ACTIVO REVALORIZADO POR EL EXPERTO</h5>
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <label for="descripcion">VALOR REVALUADO</label>
+                            <input type="text" name="descripcion" class="form-control" value="{{ $revalorizacion->valor }} Bs" id="descripcion" disabled>
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <label for="nombre">VIDA UTIL REVALUADO</label>
+                            <input type="text" name="nombre" class="form-control" value="{{ $revalorizacion->tiempo_vida }} años" id="nombre" disabled>
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <label for="estado">ESTADO</label>
+                            <input type="text" name="estado" class="form-control" value="{{ $revalorizacion->estado }}" id="descripcion" disabled>
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <label for="estado">FECHA SOLICITUD</label>
+                            <input type="text" name="estado" class="form-control" value="{{ $revalorizacion->created_at }}" id="descripcion" disabled>
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <label for="costo_revaluo">COSTO DE LA REVALUACION</label>
+                            <input type="text" name="costo_revaluo" class="form-control" value="{{ $revalorizacion->costo_revaluo }} Bs" id="descripcion" disabled>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--<h3>BOTON PARA MOSTRAR IMAGEN DEL INFORME TECNICO </h3>-->
+            <div>
+                <center>
+                    <img src="{{ asset($revalorizacion->foto) }}" width="350" height="400" class="img-circle" />
+                    <br>
+                    <label for="name">Vista detallada de </label>
+                </center>
+            </div>
+
+        </div>
+        <br>
+        <center>
+            <a class="btn btn-warning btb-sm text-light" href="{{ route('revalorizacion.index') }}">Volver</a>
+            <a href="{{ route('revalorizacion.edit', $revalorizacion->id) }}" class="btn btn-primary btb-sm text-light">
+                Editar </a>
+        </center>
     </div>
+</div>
 @stop
 @section('css')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
 @stop
 
 @section('js')
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#categoria').DataTable();
-        });
-    </script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#categoria').DataTable();
+    });
+
+</script>
 @stop
